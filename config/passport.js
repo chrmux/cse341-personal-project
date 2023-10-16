af912c82ce5037c88a3a6d805df457fcf92a2335
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2023-10-16 20:37:38 +0800

    Add files via upload

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,38 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/Auth_User');
 
+module.exports = function(passport) {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: ''
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        console.log(profile.emails[0].value);
+        const newUser = {
+            googleId: profile.id,
+            email: profile.emails[0].value,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName
+        }
+
+        try {
+            let user = await User.findOne({ googleId: profile.id });
+            if(user) {
+                done(null, user);
+            } else {
+                user = await User.create(newUser);
+                done(null, user);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }));
+
+    passport.serializeUser((user, done) => done(null, user.id));
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => done(err, user));
+    });
+}
