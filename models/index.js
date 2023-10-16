4758693ef0bf771489f4b5648960816b8a83dda9
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2023-10-16 20:34:43 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,12 @@
+const dbConfig = require("../config/db.config.js");
 
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+db.projects = require("./cars.js")(mongoose);
+db.users = require("./clients.js")(mongoose);
+
+module.exports = db;
